d5daa4c5c19f4a33ae35bcb02cf19b91f2482b9e
Xiang Zhang <xmzhang@thoughtworks.com>
2016-12-22 14:17:59 +0800

    Set the code to ready for refactor status.

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,19 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+}
