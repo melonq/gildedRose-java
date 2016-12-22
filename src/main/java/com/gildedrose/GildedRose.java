029103e748f057c29a6648874d5a04fb09fcec70
Xiang Zhang <xmzhang@thoughtworks.com>
2016-12-22 14:25:37 +0800

    Set the basic classes

---
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -0,0 +1,13 @@
+package com.gildedrose;
+
+class GildedRose {
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        // Insert your code here.
+    }
+}
